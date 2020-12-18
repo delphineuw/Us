01eb8cc7455adfc9bc4382c05f9953a6260c2ef5
sanaa jebroun <sanaajebroun@gmail.com>
2020-12-18 13:42:37 +0100

    commit

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 // Local imports
-// import AllScreen from './app/screens/EventsScreen/index'
- import AuthNavigation from './app/navigation/AuthNavigation'
 
 const App = () => {
-  return <View style={{ flex: 1 }}> 
-            <AuthNavigation />
-         </View>;
+  return <View style={{flex:1}}  > 
+         </View>
 };
 
 export default App;
