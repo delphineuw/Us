e20f36cbca0fd32e508128ca5e790a6e53fea2f0
Antoine <ant.g@hotmail.be>
2020-12-18 09:46:35 +0100

    partcicipant list

--- frontend/App.js
+++ frontend/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 // Local imports
-import ParticipantsScreen from './app/screens/ParticipantsScreen/index';
+
 
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <ParticipantsScreen />
+     
     </View>
   );
 };
