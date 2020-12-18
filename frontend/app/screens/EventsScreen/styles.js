0f1637269cd83ecb233ae66ee67026ad8190a15d
sanaa jebroun <sanaajebroun@gmail.com>
2020-12-18 13:45:56 +0100

    commit

--- frontend/app/screens/EventsScreen/styles.js
+++ frontend/app/screens/EventsScreen/styles.js
@@ -7,13 +7,10 @@ const styles = StyleSheet.create({
       flexWrap:'wrap',
       marginTop: 10,
       justifyContent: 'center'
-
     },
     events0: {
     backgroundColor: "#047C7C",
-
   },
-
   text: {
     textAlign:'center',
     fontSize: 20,
