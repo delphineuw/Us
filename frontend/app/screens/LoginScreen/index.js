87fdacc9f20f59e60dbeeb2ccd99eefae7987b20
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-03 19:18:06 +0100

    Login Screen

--- frontend/app/screens/LoginScreen/index.js
+++ frontend/app/screens/LoginScreen/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 // Local imports
+import logo from '../../assets/logo.png';
+import Form from '../../components/FormTemplate/index';
 import styles from './styles';
+
+const loginForm = [
+  {
+    id: 1,
+    render: 'text',
+    placeholder: 'Write here...',
+    label: 'Email',
+    multiline: false,
+    type: 'emailAddress',
+    security: false,
+    autoCapitalize: 'none',
+    value: ''
+  },
+  {
+    id: 2,
+    render: 'text',
+    placeholder: 'Write here...',
+    label: 'Password',
+    multiline: false,
+    type: 'password',
+    security: true,
+    autoCapitalize: 'none',
+    value: ''
+  }
+];
 
 const LoginScreen = props => {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Image style={styles.logo} source={logo} />
+      <Form inputs={loginForm} submitHandler={() => {}} />
     </View>
   );
 };
