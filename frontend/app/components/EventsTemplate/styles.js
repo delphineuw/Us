9b1138dd7118df98827abb44dfd715e8accc962a
Antoine <ant.g@hotmail.be>
2021-01-10 19:45:09 +0100

    event screen buid up

---
+++ frontend/app/components/EventsTemplate/styles.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  },
+  cardsWrapper: {
+    marginTop: 20,
+    width: '80%',
+    alignSelf: 'center',
+  },
+  card: {
+    height: 100,
+    marginVertical: 10,
+    flexDirection: 'row',
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  cardImgWrapper: {
+    flex: 1,
+  },
+  cardImg: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  cardInfo: {
+    flex: 2,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+    backgroundColor: '#fff',
+  },
+  cardTitle: {
+    fontWeight: 'bold',
+  },
+  cardDetails: {
+    fontSize: 12,
+    color: '#444',
+  },
+});
+
+export default styles;
--- frontend/app/screens/EventScreen/styles.js
+++ frontend/app/screens/EventScreen/styles.js
@@ -1,5 +1,23 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({ container: { flex: 1, justifyContent: 'center', alignItems: 'center' } });
+const styles = StyleSheet.create({ 
+    container: { 
+        flex: 1, 
+    },
+    head: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 250,
+        backgroundColor: '#rgba(21,133,130, 0.8)'
+    },
+    imgLogo: {
+        width: 150,
+        height: 150,
+        zIndex: -1,
+        position: 'absolute'
+    }
+});
 
 export default styles;
