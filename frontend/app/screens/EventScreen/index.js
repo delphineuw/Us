63a862af48082df0f5ca58d2a86d99526658376b
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-11 11:29:46 +0100

    fix

--- frontend/app/screens/EventScreen/index.js
+++ frontend/app/screens/EventScreen/index.js
@@ -8,34 +8,32 @@ import EventsTemplate from '../../components/EventsTemplate/index.js';
 import eventTest from '../../utils/eventTest.js';
 import hobbiesTest from '../../utils/hobbiesTest.js';
 
-
 const EventScreen = props => {
-
   const maping = () => {
-    return eventTest.map(item => 
-              <EventsTemplate 
-                      key={item.key} 
-                      imageUri={item.imageUri} 
-                      title={item.title} 
-                      time={item.time} 
-                      description={item.description} 
-                      />);
+    return eventTest.map(item => (
+      <EventsTemplate
+        key={item.key}
+        imageUri={item.imageUri}
+        title={item.title}
+        time={item.time}
+        description={item.description}
+      />
+    ));
   };
 
   return (
     <ScrollView style={styles.container}>
       <View style={styles.head}>
-        <Image style={styles.imgLogo} source={require('../../assets/logo.png')}/>
+        <Image style={styles.imgLogo} source={require('../../assets/logo.png')} />
         {/* <Text>Events</Text> */}
         {/* <EventsTemplate /> */}
       </View>
-      
-      {maping()}
-     
 
-      {/* <Button title="EDIT" onPress={() => props.navigation.navigate('EventEditScreen')} />
+      {maping()}
+
+      <Button title="EDIT" onPress={() => props.navigation.navigate('EventEditScreen')} />
       <Button title="Description" onPress={() => props.navigation.navigate('EventDescriptionScreen')} />
-      <Button title="Participant" onPress={() => props.navigation.navigate('ParticipantScreen')} /> */}
+      <Button title="Participant" onPress={() => props.navigation.navigate('ParticipantScreen')} />
     </ScrollView>
   );
 };
