8a6e9932ffc0c0729435e3cbe954d74e6d3dd677
sanaa jebroun <sanaajebroun@gmail.com>
2021-01-13 10:19:11 +0100

    crud api

--- frontend/app/components/UserTemplate/index.js
+++ frontend/app/components/UserTemplate/index.js
@@ -6,54 +6,30 @@ import { Entypo } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
 
 const UserTemplate = ({
-  imageUri,
-  firstName,
-  lastName,
-  age,
-  country,
-  city,
-  location,
-  hobby1,
-  hobby2,
-  hobby3,
-  imageHobby1,
-  imageHobby2,
-  imageHobby3
+  key, 
+  image, 
+  fullName, 
+  age
 }) => {
   return (
     <View style={styles.container}>
-      <Image source={imageUri} style={styles.image} />
+      <Image source={image} style={styles.image} />
       <Text style={styles.text}>
-        {firstName} {lastName}
+       {fullName}
       </Text>
       <View style={styles.gridUser}>
         <FontAwesome name="birthday-cake" size={24} color="black" />
-        <Text style={styles.textcontent}> {age} years old </Text>
+        <Text style={styles.textcontent}> 26 years old </Text>
       </View>
       <View style={styles.gridUser}>
         <Entypo name="location-pin" size={24} color="black" />
-        <Text style={styles.textcontent}>
-          {country}, {city}
-        </Text>
+        <Text style={styles.textcontent}> Bruxelles </Text>
       </View>
       <View style={styles.gridUser}>
         <Fontisto name="world-o" size={24} color="black" />
-        <Text style={styles.textcontent}> {location} </Text>
+        <Text style={styles.textcontent}> Italie </Text>
       </View>
-      <View style={styles.hobbies}>
-        <View style={styles.gridHobby}>
-          <Image source={imageHobby1} style={{ height: 100, width: 100, borderRadius: 100 }} />
-          <Text style={styles.texthobby}> {hobby1} </Text>
-        </View>
-        <View style={styles.gridHobby}>
-          <Image source={imageHobby2} style={{ height: 100, width: 100, borderRadius: 100 }} />
-          <Text style={styles.texthobby}> {hobby2} </Text>
-        </View>
-        <View style={styles.gridHobby}>
-          <Image source={imageHobby3} style={{ height: 100, width: 100, borderRadius: 100 }} />
-          <Text style={styles.texthobby}> {hobby3}</Text>
-        </View>
-      </View>
+     
     </View>
   );
 };
