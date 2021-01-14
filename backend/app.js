7d636eb606db2524d3ca123cc5cf1b836ee3b65a
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-14 13:39:19 +0100

    Admin Auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const AdminBro = require('admin-bro');
 const AdminBroExpress = require('@admin-bro/express');
 const AdminBroSequelize = require('@admin-bro/sequelize');
@@ -21,8 +22,32 @@ const adminBro = new AdminBro({
   databases: [db],
   rootPath: '/admin'
 });
-const router = AdminBroExpress.buildRouter(adminBro);
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+  authenticate: async (email, password) => {
+    const admin = await db.Admin.findOne({ where: { email } });
+    if (admin) {
+      const matched = await bcrypt.compare(password, admin.password);
+      if (matched) {
+        return admin;
+      }
+    }
+    return false;
+  },
+  cookiePassword: 'some-secret-password-used-to-secure-cookie'
+});
 app.use(adminBro.options.rootPath, router);
+
+const createAdmin = async (email, password) => {
+  const adminExists = await db.Admin.findOne({ where: { email } });
+  if (adminExists) return;
+
+  const newAdmin = db.Admin.build({ email, password: await bcrypt.hash(password, 10) });
+  try {
+    await newAdmin.save();
+  } catch (error) {
+    console.log('Could not create Admin.');
+  }
+};
 
 // Registered Routes
 app.use('/api', routes);
@@ -36,6 +61,7 @@ app.use((error, req, res, next) => {
 // Server Start & Database connection
 sequelize
   .authenticate()
+  .then(() => createAdmin(process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD))
   .then(() =>
     app.listen(process.env.PORT || 5000, () =>
       console.log(`Server up and running on port: ${process.env.PORT || 5000}!`)
