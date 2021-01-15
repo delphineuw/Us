f730a851d69754ba92502e065d3ae968beb85766
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-15 13:49:32 +0100

    Fix

--- frontend/app/screens/ProfileScreen/index.js
+++ frontend/app/screens/ProfileScreen/index.js
@@ -1,44 +1,32 @@
-import React, { useState, useEffect} from 'react'
-import {View, Text, Button} from 'react-native'
-import axios from 'axios'
-import UserTemplate from '../../components/UserTemplate/index'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import axios from 'axios';
+import UserTemplate from '../../components/UserTemplate/index';
 
+const profileScreen = props => {
+  const [user, setUser] = useState([]);
+  // useEffect(() => {
+  // const {id} = this.props.match.params
+  //  axios.get(`http://localhost:4000/api/users/`)
+  //     .then((res) => {
+  //         setUser(res.data);
+  //     }).catch((err) => {
+  //         console.log(err);
+  //     })
+  // }, []);
 
-const profileScreen = () => {
-    const [user, setUser] = useState([]);
-    useEffect(() => {
-        // const {id} = this.props.match.params
-         axios.get(`http://localhost:4000/api/users/`)
-            .then((res) => { 
-                setUser(res.data);
-            }).catch((err) => {
-                console.log(err);
-            })         
-        }, []);
+  const maping = () => {
+    return user.map(item => (
+      <UserTemplate id={item.id} image={item.image} fullName={item.fullName} age={item.birthday} />
+    ));
+  };
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* {maping()} */}
 
-        const maping = () => {
-            return user.map(item => (
-              <UserTemplate
-                id = {item.id}
-                image = {item.image}
-                fullName = {item.fullName}
-                age = {item.birthday}
-              />
-            ));
-          };
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        {maping()}
-
-        <Button title="hobbies" onPress={() => {}} />
-            <Button title="edit" onPress={() => {}} />
-        </View>
-    )
-}
-export default profileScreen
-
-
-
-
-
-
+      <Button title="hobbies" onPress={() => props.navigation.navigate('EditHobbiesScreen')} />
+      <Button title="edit" onPress={() => props.navigation.navigate('EditProfileScreen')} />
+    </View>
+  );
+};
+export default profileScreen;
