46a826b867d1720ae9594d9ad1378408d088de20
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-02-03 12:16:09 +0100

    map

--- frontend/app/components/MiniMap/index.js
+++ frontend/app/components/MiniMap/index.js
@@ -1,73 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import { View, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { TabRouter } from "@react-navigation/native";
 import axios from 'axios';
 
 // Local imports
 import styles from './styles';
-import eventTest from '../../utils/eventTest.js';
 
-
-const MiniMap = ({event}) => {
-
-  const API_KEY = 'AIzaSyDbPBhjyCIyVvmR4B-27U-e36dKJ-MxUMU';
-
-  const getCoordsForAddress = async ({event}) => {
-    console.log('Im here')
-    
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(event.location)}&key=${API_KEY}`)
-    
-      const data = response.data ;
-
-
-      if ( !data || data.status === 'ZERO_RESULTS') {
-        const error = new Errors('No address found', 422);
-        throw error
-      };
-
-      const coordinates = data.results[0].geometry.location;
-      console.log(coordinate)
-  
-      return coordinates
-
-  }
-
+const MiniMap = ({ event }) => {
   const { width, height } = Dimensions.get('window');
-  const ASPECT_RATIO = width / height;
-  const LATITUDE = 50.84615802838505;
-  const LONGITUDE = 4.353924849306058;
-  // const LATITUDE = 50.84615802838505;
-  // const LONGITUDE = 4.353924849306058;
-  const LATITUDE_DELTA = 0.0052;
-  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+  const API_KEY = 'APIKEY';
 
+  const [coordinates, setCoordinates] = useState({});
   const [region, setRegion] = useState({
-    latitude: LATITUDE,
-    longitude: LONGITUDE,
-    latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA
+    latitude: 50.84615802838505,
+    longitude: 4.353924849306058,
+    latitudeDelta: 0.0192,
+    longitudeDelta: 0.0052 * (width / height)
   });
 
-  return (
+  useEffect(() => {
+    (async () => {
+      let response;
+      try {
+        response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+            event.location
+          )}&key=${API_KEY}`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+      if (response) {
+        setCoordinates(response.data.results[0].geometry.location);
+        setRegion(prev => ({
+          ...prev,
+          latitude: response.data.results[0].geometry.location.lat,
+          longitude: response.data.results[0].geometry.location.lng
+        }));
+      }
+    })();
+  }, []);
 
-      <View style={styles.container}>
-        <MapView 
-            style={styles.map} 
-            // provider={MapView.PROVIDER_GOOGLE}
-            initialRegion={region}
-            showsUserLocation={true}
-            onRegionChangeComplete={region => setRegion(region)}
-            >
-          {/* <Marker 
-              coordinate={coordinate}
-              title={event.title}
-              description={event.location} 
-          /> */}
-        </MapView>
-      </View>
-    
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={region}
+        showsUserLocation={true}
+        onRegionChangeComplete={region => setRegion(region)}
+      >
+        <Marker
+          coordinate={{ latitude: coordinates.lat, longitude: coordinates.lng }}
+          title={event.title}
+          description={event.location}
+        />
+      </MapView>
+    </View>
   );
-}
+};
 
 export default MiniMap;
